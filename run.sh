74f2e67e7f13a9523e7f49810479d33635f99414
NABESHIMA Hidetomo <nabesima@yamanashi.ac.jp>
2020-04-24 16:44:57 +0900

    Refactered run.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-##/usr/sbin/sshd -D &
 
-##PATH="$PATH:/opt/openmpi/bin/"
 BASENAME="${0##*/}"
 log () {
-  echo "${BASENAME} - ${1}"
+  echo "${BASENAME}: ${1}"
 }
-##HOST_FILE_PATH="/tmp/hostfile"
-#aws s3 cp $S3_INPUT $SCRATCH_DIR
-#tar -xvf $SCRATCH_DIR/*.tar.gz -C $SCRATCH_DIR
 
-## Detect the number of physical cores
-NUM_PHYSICAL_CORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
-log "# of physical cores = $NUM_PHYSICAL_CORES"
+# Set child by default switch to main if on main node container
+NODE_TYPE="child"
+if [ "${AWS_BATCH_JOB_MAIN_NODE_INDEX}" == "${AWS_BATCH_JOB_NODE_INDEX}" ]; then
+  NODE_TYPE="main"
+fi
 
-sleep 2
-##echo main node: ${AWS_BATCH_JOB_MAIN_NODE_INDEX}
-##echo this node: ${AWS_BATCH_JOB_NODE_INDEX}
-echo Downloading problem from S3: ${COMP_S3_PROBLEM_PATH}
+# If host is child node, then exit
+if [ "${NODE_TYPE}" = "child" ]; then
+   exit 0
+fi
 
+# Detect the number of local and physical cores
+NUM_LOGICAL_CORES=$(lscpu -p | egrep -v '^#' | wc -l)
+NUM_PHYSICAL_CORES=$(lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l)
+log "Host has ${NUM_LOGICAL_CORES} logical and ${NUM_PHYSICAL_CORES} physical cores"
+
+# If NUM_THREADS is not defined, we use NUM_PHYSICAL_CORES as NUM_THREADS
+if [ -z "${NUM_THREADS}" ]; then
+    NUM_THREADS=${NUM_PHYSICAL_CORES}
+fi
+log "Solver will invoke ${NUM_THREADS} threads"
+
+log "Downloading problem from s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH}"
 if [[ "${COMP_S3_PROBLEM_PATH}" == *".xz" ]];
 then
   aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} supervised-scripts/test.cnf.xz
@@ -27,89 +36,20 @@ else
   aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} supervised-scripts/test.cnf
 fi
 
-
 ## Main
-time /manyglucose-4.1-60/parallel/manyglucose-4.1-60 -verb=0 -model -real-time-lim=28800 -nthreads=${NUM_PHYSICAL_CORES} supervised-scripts/test.cnf
+time /manyglucose-4.1-60/parallel/manyglucose-4.1-60 -verb=0 -model -real-time-lim=5000 -nthreads=${NUM_THREADS} supervised-scripts/test.cnf > supervised-scripts/test.log 2>&1
+RET_CODE=$?
 
-exit 0
+if [ -z "${COMP_S3_RESULT_PATH}" ]; then
+    cat supervised-scripts/test.log
+else
+  aws s3 cp supervised-scripts/test.log s3://${S3_BKT}/${COMP_S3_RESULT_PATH}
+fi
 
-### Set child by default switch to main if on main node container
-##NODE_TYPE="child"
-##if [ "${AWS_BATCH_JOB_MAIN_NODE_INDEX}" == "${AWS_BATCH_JOB_NODE_INDEX}" ]; then
-##  log "Running synchronize as the main node"
-##  NODE_TYPE="main"
-##fi
+case "${RET_CODE}" in
+    "10"         ) EXIT_CODE=0 ;; # SATISFIABLE
+    "20" | "138" ) EXIT_CODE=0 ;; # UNSATISFIABLE
+    *            ) EXIT_CODE=${RET_CODE} ;;
+esac
 
-### wait for all nodes to report
-##wait_for_nodes () {
-##  log "Running as master node"
-##
-##  touch $HOST_FILE_PATH
-##  ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-##
-##  availablecores=$(nproc)
-##  log "master details -> $ip:$availablecores"
-###  echo "$ip slots=$availablecores" >> $HOST_FILE_PATH
-##  echo "$ip" >> $HOST_FILE_PATH
-##  lines=$(ls -dq /tmp/hostfile* | wc -l)
-##  while [ "${AWS_BATCH_JOB_NUM_NODES}" -gt "${lines}" ]
-##  do
-##    cat $HOST_FILE_PATH
-##    lines=$(ls -dq /tmp/hostfile* | wc -l)
-##
-##    log "$lines out of $AWS_BATCH_JOB_NUM_NODES nodes joined, check again in 1 second"
-##    sleep 1
-###    lines=$(sort $HOST_FILE_PATH|uniq|wc -l)
-##  done
-##
-##
-##  # All of the hosts report their IP and number of processors. Combine all these
-##  # into one file with the following script:
-##  python supervised-scripts/make_combined_hostfile.py ${ip}
-##  cat combined_hostfile
-##
-##  # REPLACE THE FOLLOWING LINE WITH YOUR PARTICULAR SOLVER
-##  time mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np ${AWS_BATCH_JOB_NUM_NODES} --hostfile combined_hostfile /hordesat/hordesat  -c=${NUM_PROCESSES} -t=28800 -d=7 supervised-scripts/test.cnf
-##  
-##}
-##
-### Fetch and run a script
-##report_to_master () {
-##  # get own ip and num cpus
-##  #
-##  ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-##
-##
-##  availablecores=$(nproc)
-##
-##  log "I am a child node -> $ip:$availablecores, reporting to the master node -> ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS}"
-##
-###  echo "$ip slots=$availablecores" >> $HOST_FILE_PATH${AWS_BATCH_JOB_NODE_INDEX}
-##  echo "$ip" >> $HOST_FILE_PATH${AWS_BATCH_JOB_NODE_INDEX}
-##  ping -c 3 ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS}
-##  until scp $HOST_FILE_PATH${AWS_BATCH_JOB_NODE_INDEX} ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS}:$HOST_FILE_PATH${AWS_BATCH_JOB_NODE_INDEX}
-##  do
-##    echo "Sleeping 5 seconds and trying again"
-##  done
-##  log "done! goodbye"
-##  ps -ef | grep sshd
-##  tail -f /dev/null
-##}
-####
-###
-### Main - dispatch user request to appropriate function
-##log $NODE_TYPE
-##case $NODE_TYPE in
-##  main)
-##    wait_for_nodes "${@}"
-##    ;;
-##
-##  child)
-##    report_to_master "${@}"
-##    ;;
-##
-##  *)
-##    log $NODE_TYPE
-##    usage "Could not determine node type. Expected (main/child)"
-##    ;;
-##esac
+exit ${EXIT_CODE}
